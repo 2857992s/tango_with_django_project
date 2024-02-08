d5eaed48d10b3fd3f64516620e56691ad22fb451
Arif Shuaibu <2857992s@student.gla.ac.uk>
2024-02-08 06:44:52 +0000

    commit for chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.admin.sites import AdminSite
+from rango.models import Category, Page
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display=('title','category','url')
+    def __init__(self, model: type, admin_site: AdminSite | None) -> None:
+        super().__init__(model, admin_site)
+
+        
+admin.site.register(Category)
+admin.site.register(Page,PageAdmin)
--- rango/urls.py
+++ rango/urls.py
@@ -4,6 +4,8 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
+    path(r'index.php', views.index, name='index'),
+    path(r'index.html', views.index, name='index'),
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
 ]
